bf6c12d8c408fbd57fb26e83b108b5a9047a559d
vabaly <love0and0life@gmail.com>
2019-11-12 21:17:23 +0800

    feat: auto add background to component

--- src/process.ts
+++ src/process.ts
@@ -2,8 +2,9 @@
  * @file 图像处理方法
  */
 import path from 'path';
-import gm from 'gm';
-import { pictureToXML } from 'picture-to-xml';
+import fs from 'fs';
+import gm, { Dimensions } from 'gm';
+import { pictureToXML, ComponentData } from 'picture-to-xml';
 import { OUTPUT } from './config';
 import { getCount } from './util';
 import { logError } from './log';
@@ -51,10 +52,47 @@ function getOutputPath(output: Output): string {
 }
 
 /**
+ * 随机获取背景
+ */
+function getBackgroundRandom(): string {
+    const backgroundDir = path.join(__dirname, './assets/background');
+    const backgroundFileList = fs.readdirSync(backgroundDir);
+    const length = backgroundFileList.length;
+    const index = Math.floor(Math.random() * length);
+    const filename = backgroundFileList[index];
+    const filePath = path.resolve(backgroundDir, filename);
+
+    return filePath;
+}
+
+/**
+ * 获取图像尺寸
+ * @param filePath 图像的地址，或者图像的 GM 实例
+ */
+function getSize(filePath: string): Promise<Dimensions> {
+    return new Promise((resolve) => {
+        gm(filePath)
+            .size((error, size) => {
+                if (error) {
+                    logError('获取图片的尺寸失败', error.message);
+
+                    // 没有获取到即认为文件尺寸为 0, 0
+                    resolve({
+                        width: 0,
+                        height: 0
+                    });
+                }
+
+                resolve(size);
+            });
+    });
+}
+
+/**
  * 图像生成 XML 文件
  * @param outputPath 图像输出的地址
  */
-function generateXML(outputPath: string): Promise<void> {
+function generateXML(outputPath: string, componentData: ComponentData): Promise<void> {
     const pathObject = path.parse(outputPath);
     const componentName = 'demo';
     const filename = pathObject.name;
@@ -64,7 +102,8 @@ function generateXML(outputPath: string): Promise<void> {
     return pictureToXML(
         outputPath,
         componentName,
-        XMLOutputPath
+        XMLOutputPath,
+        componentData
     );
 }
 
@@ -79,11 +118,97 @@ function handle(
 ) {
     return function callback(error: Error|null): void {
         if (error) {
+            console.log('error', error);
             reject(error);
         } else {
             resolve(true);
         }
     };
+}
+
+// 缩放
+export function resize(inputPath: string, outputPath: string, finalWidth: number): Promise<void> {
+    return new Promise((resolve, reject) => {
+        gm(inputPath)
+            .resize(finalWidth)
+            .write(outputPath, handle(resolve, reject));
+    });
+}
+
+/**
+ * 组件图片嵌入至随机选取的背景中
+ * @param inputPath 组件的路径
+ * @param outputPath 组件图片嵌入背景后输出的路径
+ */
+async function composite(inputPath: string, outputPath: string): Promise<ComponentData> {
+    let componentPath = inputPath;
+
+    // 随机选取一张背景图
+    const backgroundPath = getBackgroundRandom();
+    // 获取背景图的尺寸
+    const backgroundSize = await getSize(backgroundPath);
+    // 获取组件的尺寸
+    const componentSize = await getSize(inputPath);
+
+    let { width: componentWidth, height: componentHeight } = componentSize;
+
+    // 组件的尺寸必须小于背景图的尺寸，即宽高都小于背景图，且小于等于一定比例，目前设为 0.8
+    // 如果大于这个比例就缩小组件图的尺寸
+    const rateStandard = 0.8;
+    const rateWidth = componentSize.width / backgroundSize.width;
+    const rateHeight = componentSize.height / backgroundSize.height;
+
+    // 使用最大的比率来计算组件图缩小后的尺寸大小
+    const maxRate = Math.max(rateWidth, rateHeight);
+
+    if (maxRate > rateStandard) {
+        const deltaRate = rateStandard / maxRate;
+        const finalWidth = componentSize.width * deltaRate;
+        const finalHeight = componentSize.height * deltaRate;
+
+        // 缩小图片
+        await resize(inputPath, outputPath, finalWidth);
+
+        // 重新设置组件的路径
+        // 重新设置组件的尺寸以便后面计算
+        componentWidth = finalWidth;
+        componentHeight = finalHeight;
+
+        componentPath = outputPath;
+    }
+
+    // 在上述前提下，组件图一定能放在背景图上，但是放置的位置也有一个范围，这里就是计算这个范围的过程
+    const maxX = backgroundSize.width - componentWidth;
+    const maxY = backgroundSize.height - componentHeight;
+
+    // 随机选取一个坐标来放置
+    const x = Math.floor(Math.random() * maxX);
+    const y = Math.floor(Math.random() * maxY);
+    const xmax = x + componentWidth;
+    const ymax = y + componentHeight;
+
+    // 开始图片拼接了
+    return new Promise((resolve, reject) => {
+        gm(backgroundPath)
+            .composite(componentPath)
+            .geometry(`+${x}+${y}`)
+            .write(outputPath, handle(resolve, reject));
+    })
+        .then(() => {
+            // 返回 XML 文件需要知道的坐标信息
+            return {
+                box: {
+                    xmin: x,
+                    xmax,
+                    ymin: y,
+                    ymax
+                },
+                size: {
+                    width: componentWidth,
+                    height: componentHeight
+                }
+            };
+        });
 }
 
 /**
@@ -99,7 +224,7 @@ export function rotate(input: ImageInput): Promise<any> {
             .rotate('white', 90)
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        // .then(() => generateXML(outputPath))
         .catch((error: Error): boolean => {
             logError(`旋转图片 ${path} 时发生错误`, error.message);
             return false;
@@ -120,7 +245,7 @@ export function flip(input: ImageInput): Promise<any> {
             .flip()
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        // .then(() => generateXML(outputPath))
         .catch((error: Error): boolean => {
             logError(`沿 Y 轴翻转图片 ${path} 时发生错误`, error.message);
             return false;
@@ -140,7 +265,7 @@ export function flop(input: ImageInput): Promise<any> {
             .flop()
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        // .then(() => generateXML(outputPath))
         .catch((error: Error): boolean => {
             logError(`沿 X 轴翻转图片 ${path} 时发生错误`, error.message);
             return false;
@@ -161,7 +286,7 @@ export function flipAndFlop(input: ImageInput): Promise<any> {
             .flop()
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        // .then(() => generateXML(outputPath))
         .catch((error: Error): boolean => {
             logError(`沿对角线翻转图片 ${path} 时发生错误`, error.message);
             return false;
@@ -173,16 +298,18 @@ export function flipAndFlop(input: ImageInput): Promise<any> {
  * @param input 输入的图像信息
  * @param delta 图像亮度的值，基准值是 100，比 100 高则是增加亮度，比 100 低则是减少亮度
  */
-export function adjustBrightness(input: ImageInput, brightness: number): Promise<any> {
+export async function adjustBrightness(input: ImageInput, brightness: number): Promise<any> {
     const { path, name, ext } = input;
     const outputPath = getOutputPath({ name, ext, modifier: `adjust-brightness-${brightness}` });
 
+    const componentData = await composite(path, outputPath);
+
     return new Promise((resolve, reject): void => {
-        gm(path)
+        gm(outputPath)
             .modulate(brightness, 100, 100)
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        .then(() => generateXML(outputPath, componentData))
         .catch(() => false);
 }
 
@@ -191,16 +318,18 @@ export function adjustBrightness(input: ImageInput, brightness: number): Promise
  * @param input 输入的图像信息
  * @param delta 图像亮度的值，基准值是 100，比 100 高则是增加亮度，比 100 低则是减少亮度
  */
-export function adjustContrast(input: ImageInput, multiplier: number): Promise<any> {
+export async function adjustContrast(input: ImageInput, multiplier: number): Promise<any> {
     const { path, name, ext } = input;
     const outputPath = getOutputPath({ name, ext, modifier: `adjust-contrast-${multiplier}` });
 
+    const componentData = await composite(path, outputPath);
+
     return new Promise((resolve, reject): void => {
-        gm(path)
+        gm(outputPath)
             .contrast(multiplier)
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        .then(() => generateXML(outputPath, componentData))
         .catch(() => false);
 }
 
@@ -209,16 +338,18 @@ export function adjustContrast(input: ImageInput, multiplier: number): Promise<a
  * @param input 输入的图像信息
  * @param hue 图像色相的值，基准值是 100，比 100 高则是增加色相，比 100 低则是减少色相
  */
-export function adjustHue(input: ImageInput, hue: number): Promise<any> {
+export async function adjustHue(input: ImageInput, hue: number): Promise<any> {
     const { path, name, ext } = input;
     const outputPath = getOutputPath({ name, ext, modifier: `adjust-hue-${hue}` });
 
+    const componentData = await composite(path, outputPath);
+
     return new Promise((resolve, reject): void => {
-        gm(path)
+        gm(outputPath)
             .modulate(100, 100, hue)
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        .then(() => generateXML(outputPath, componentData))
         .catch(() => false);
 }
 
@@ -227,16 +358,18 @@ export function adjustHue(input: ImageInput, hue: number): Promise<any> {
  * @param input 输入的图像信息
  * @param saturation 图像饱和度的值，基准值是 100，比 100 高则是增加饱和度，比 100 低则是减少饱和度
  */
-export function adjustSaturation(input: ImageInput, saturation: number): Promise<any> {
+export async function adjustSaturation(input: ImageInput, saturation: number): Promise<any> {
     const { path, name, ext } = input;
     const outputPath = getOutputPath({ name, ext, modifier: `adjust-saturation-${saturation}` });
 
+    const componentData = await composite(path, outputPath);
+
     return new Promise((resolve, reject): void => {
-        gm(path)
+        gm(outputPath)
             .modulate(100, saturation, 100)
             .write(outputPath, handle(resolve, reject));
     })
-        .then(() => generateXML(outputPath))
+        .then(() => generateXML(outputPath, componentData))
         .catch(() => false);
 }
 
